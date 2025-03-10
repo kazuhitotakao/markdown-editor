cc11cf89613ae0b9b02e711a17d340f771fdcf54
kazuhitotakao <simasyamu@gmail.com>
2025-03-10 22:20:24 +0900

    webworkerを導入

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -7,8 +7,10 @@ import { Button } from '../components/button';
 import { SaveModal } from '../components/save_modal';
 import { Link } from 'react-router-dom';
 import { Header } from '../components/header';
+import ConvertMarkdownWorker from 'worker-loader!../worker/convert_markdown_worker';
 
-const { useState } = React;
+const convertMarkdownWorker = new (ConvertMarkdownWorker);
+const { useState, useEffect } = React;
 
 const Wrapper = styled.header`
   bottom: 0;
@@ -55,8 +57,18 @@ interface Props {
 
 export const Editor: React.FC<Props> = (props) => {
   const { text, setText } = props;
-
   const [showModal, setShowModal] = useState(false);
+  const [html, setHtml] = useState('');
+
+  useEffect(() => {
+    convertMarkdownWorker.onmessage = (event) => {
+      setHtml(event.data.html);
+    };
+  }, []);
+
+  useEffect(() => {
+    convertMarkdownWorker.postMessage(text);
+  }, [text]);
 
   return (
     <>
@@ -72,7 +84,7 @@ export const Editor: React.FC<Props> = (props) => {
           value={text}
         />
         <Preview>
-          <ReactMarkdown>{text}</ReactMarkdown>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
         </Preview>
       </Wrapper>
       {showModal && (
